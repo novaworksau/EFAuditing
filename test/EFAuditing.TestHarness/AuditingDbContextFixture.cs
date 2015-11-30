df4432aad8b68bd80a68b1f0c55f6d105dd1a24f
Darren Neimke <dneimke@oconics.com>
2015-11-30 13:22:15 +1030

    Reanabling Auditing tests

--- test/EFAuditing.TestHarness/AuditingDbContextFixture.cs
+++ test/EFAuditing.TestHarness/AuditingDbContextFixture.cs
@@ -10,109 +10,109 @@ using Xunit;
 
 namespace EFAuditing.TestHarness
 {
-    //public class AuditingDbContextFixture
-    //{
-    //    protected IServiceProvider _provider = null;
-    //    protected string _currentUser = null;
+    public class AuditingDbContextFixture
+    {
+        protected IServiceProvider _provider = null;
+        protected string _currentUser = null;
 
-    //    public AuditingDbContextFixture()
-    //    {
-    //        var fixture = new InMemoryFixture();
-    //        _provider = fixture.GetServiceProvider();
-    //        _currentUser = Thread.CurrentPrincipal.Identity.Name;
-    //    }
-
-        
-
-    //    [Fact]
-    //    public void ShouldAddData()
-    //    {
-    //        using (var db = _provider.GetService<TestDbContext>())
-    //        {
-    //            // Arrange
-    //            db.SeedTestData();
-    //            var expectedCount = 5;
-
-    //            // Act
-    //            var customer = new Customer { CustomerId = 5, FirstName = "Misty", LastName = "Shock" };
-    //            db.Customers.Add(customer);
-    //            db.SaveChanges(_currentUser);
-    //            var actualCount = db.Customers.Count();
-
-    //            // Assert
-    //            Assert.Equal(expectedCount, actualCount);
-    //        }
-    //    }
+        public AuditingDbContextFixture()
+        {
+            var fixture = new InMemoryFixture();
+            _provider = fixture.GetServiceProvider();
+            _currentUser = "dneimke"; // Thread.CurrentPrincipal.Identity.Name;
+        }
 
 
-    //    [Fact]
-    //    public void ShouldRetrieveData()
-    //    {
-    //        using (var db = _provider.GetService<TestDbContext>())
-    //        {
-    //            // Arrange
-    //            db.SeedTestData();
-    //            var expectedCount = 4;
+
+        [Fact]
+        public void ShouldAddData()
+        {
+            using (var db = _provider.GetService<TestDbContext>())
+            {
+                // Arrange
+                db.SeedTestData();
+                var expectedCount = 5;
+
+                // Act
+                var customer = new Customer { CustomerId = 5, FirstName = "Misty", LastName = "Shock" };
+                db.Customers.Add(customer);
+                db.SaveChanges(_currentUser);
+                var actualCount = db.Customers.Count();
+
+                // Assert
+                Assert.Equal(expectedCount, actualCount);
+            }
+        }
 
 
-    //            // Act
-    //            var actualCount = db.Customers.Count();
-
-    //            // Assert
-    //            Assert.Equal(expectedCount, actualCount);
-    //        }
-    //    }
-
-
-    //    [Fact]
-    //    public void ShoulDeleteData()
-    //    {
-    //        using (var db = _provider.GetService<TestDbContext>())
-    //        {
-    //            // Arrange
-    //            db.SeedTestData();
-    //            var expectedCount = 3;
+        [Fact]
+        public void ShouldRetrieveData()
+        {
+            using (var db = _provider.GetService<TestDbContext>())
+            {
+                // Arrange
+                db.SeedTestData();
+                var expectedCount = 4;
 
 
-    //            // Act
-    //            var cust = db.Customers.FirstOrDefault();
-    //            db.Customers.Remove(cust);
-    //            db.SaveChanges(_currentUser);
-    //            var actualCount = db.Customers.Count();
+                // Act
+                var actualCount = db.Customers.Count();
 
-    //            // Assert
-    //            Assert.Equal(expectedCount, actualCount);
-    //        }
-    //    }
+                // Assert
+                Assert.Equal(expectedCount, actualCount);
+            }
+        }
 
 
-    //    [Fact]
-    //    public void ShouldCreateAuditLogs()
-    //    {
-    //        using (var db = _provider.GetService<TestDbContext>())
-    //        {
-    //            // Arrange
-    //            var expectedCount = 1;
-
-    //            // Act
-    //            var customer = new Customer { CustomerId = 5, FirstName = "Misty", LastName = "Shock" };
-    //            db.Customers.Add(customer);
-    //            db.SaveChanges(_currentUser);
-    //            var auditLogs = db.GetAuditLogs().ToList();
-    //            var first = auditLogs.FirstOrDefault() as CustomAuditLog;
-
-    //            // Assert
-    //            Assert.Equal(expectedCount, auditLogs.Count);
-    //            Assert.NotNull(first);
-
-    //            var differences = first.Differences;
-    //            var diff = JsonConvert.DeserializeObject<List<PropertyDiff>>(differences);
-
-    //            Assert.Equal(diff.Count, 3); // should be 3 fields -> Id, FirstName, LastName
-
-    //        }
-    //    }
+        [Fact]
+        public void ShoulDeleteData()
+        {
+            using (var db = _provider.GetService<TestDbContext>())
+            {
+                // Arrange
+                db.SeedTestData();
+                var expectedCount = 3;
 
 
-    //}
+                // Act
+                var cust = db.Customers.FirstOrDefault();
+                db.Customers.Remove(cust);
+                db.SaveChanges(_currentUser);
+                var actualCount = db.Customers.Count();
+
+                // Assert
+                Assert.Equal(expectedCount, actualCount);
+            }
+        }
+
+
+        [Fact]
+        public void ShouldCreateAuditLogs()
+        {
+            using (var db = _provider.GetService<TestDbContext>())
+            {
+                // Arrange
+                var expectedCount = 1;
+
+                // Act
+                var customer = new Customer { CustomerId = 5, FirstName = "Misty", LastName = "Shock" };
+                db.Customers.Add(customer);
+                db.SaveChanges(_currentUser);
+                var auditLogs = db.GetAuditLogs().ToList();
+                var first = auditLogs.FirstOrDefault() as CustomAuditLog;
+
+                // Assert
+                Assert.Equal(expectedCount, auditLogs.Count);
+                Assert.NotNull(first);
+
+                var differences = first.Differences;
+                var diff = JsonConvert.DeserializeObject<List<PropertyDiff>>(differences);
+
+                Assert.Equal(diff.Count, 3); // should be 3 fields -> Id, FirstName, LastName
+
+            }
+        }
+
+
+    }
 }
