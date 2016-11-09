ed534d05f61da4434c9912cb04b1f1517d9c282e
Darren Neimke <dneimke@novaworks.com.au>
2016-11-09 13:47:05 +1030

    Adding Tests back in to try and get build working

--- test/EFAuditing.TestHarness/AutoLogBuilderFixture.cs
+++ test/EFAuditing.TestHarness/AutoLogBuilderFixture.cs
@@ -13,79 +13,79 @@ namespace EFAuditing.TestHarness
     public class AutoLogBuilderFixture
     {
 
-        //protected IServiceProvider _provider = null;
-        //protected string _currentUser = null;
-        //List<AuditLog> _auditLogList;
+        protected IServiceProvider _provider = null;
+        protected string _currentUser = null;
+        List<AuditLog> _auditLogList;
 
-        //public AutoLogBuilderFixture()
-        //{
-        //    var fixture = new InMemoryFixture();
-        //    _provider = fixture.GetServiceProvider();
-        //    _currentUser = "tsmith"; // Thread.CurrentPrincipal.Identity.Name;
-        //    _auditLogList = new List<AuditLog>();
-        //}
+        public AutoLogBuilderFixture()
+        {
+            var fixture = new InMemoryFixture();
+            _provider = fixture.GetServiceProvider();
+            _currentUser = "tsmith"; // Thread.CurrentPrincipal.Identity.Name;
+            _auditLogList = new List<AuditLog>();
+        }
 
-        //[Fact]
-        //public void GetAddedAuditLogsTest()
-        //{
-        //    //This test ensures only one log is created for an Added Entity Entry
-        //    using (var db = _provider.GetService<TestDbContext>())
-        //    {
-        //        //Arrange
-        //        var customer = new Customer { CustomerId = 5, FirstName = "Trevor", LastName = "Smith" };
-        //        db.Customers.Add(customer);
-        //        var addedEntityEntry = db.ChangeTracker.Entries().Where(p => p.State == EntityState.Added).First();
+        [Fact]
+        public void GetAddedAuditLogsTest()
+        {
+            //This test ensures only one log is created for an Added Entity Entry
+            using (var db = _provider.GetService<TestDbContext>())
+            {
+                //Arrange
+                var customer = new Customer { CustomerId = 5, FirstName = "Trevor", LastName = "Smith" };
+                db.Customers.Add(customer);
+                var addedEntityEntry = db.ChangeTracker.Entries().Where(p => p.State == EntityState.Added).First();
 
-        //        //Act
-        //        var result = AuditLogBuilder.GetAddedAuditLogs(addedEntityEntry, _currentUser, EntityState.Added);
-        //        _auditLogList.Add(result);
+                //Act
+                var result = AuditLogBuilder.GetAddedAuditLogs(addedEntityEntry, _currentUser, EntityState.Added);
+                _auditLogList.Add(result);
 
-        //        //Assert
-        //        Assert.Equal(1, _auditLogList.Count());
-        //    }
-        //}
+                //Assert
+                Assert.Equal(1, _auditLogList.Count());
+            }
+        }
 
-        //[Fact]
-        //public void GetModifiedAuditLogsTest()
-        //{
-        //    //This test ensures that one log is create for each property changed
-        //    using (var db = _provider.GetService<TestDbContext>())
-        //    {
-        //        //Arrange
-        //        db.SeedTestData();
-        //        var customer = db.Customers.First();
-        //        customer.FirstName = "Susan";
-        //        customer.LastName = "Smith";
-        //        var modifiedEntry = db.ChangeTracker.Entries().Where(p => p.State == EntityState.Modified).First();
+        [Fact]
+        public void GetModifiedAuditLogsTest()
+        {
+            //This test ensures that one log is create for each property changed
+            using (var db = _provider.GetService<TestDbContext>())
+            {
+                //Arrange
+                db.SeedTestData();
+                var customer = db.Customers.First();
+                customer.FirstName = "Susan";
+                customer.LastName = "Smith";
+                var modifiedEntry = db.ChangeTracker.Entries().Where(p => p.State == EntityState.Modified).First();
 
-        //        //Act
-        //        var result = AuditLogBuilder.GetAuditLogs(modifiedEntry, _currentUser, EntityState.Modified);
-        //        _auditLogList.AddRange(result);
+                //Act
+                var result = AuditLogBuilder.GetAuditLogs(modifiedEntry, _currentUser, EntityState.Modified);
+                _auditLogList.AddRange(result);
 
-        //        //Assert
-        //        Assert.Equal(2, _auditLogList.Count());
-        //    }
-        //}
+                //Assert
+                Assert.Equal(2, _auditLogList.Count());
+            }
+        }
 
-        //[Fact]
-        //public void GetDeletedAuditLogsTest()
-        //{
-        //    //This test ensures only one log is created for a Deleted Entity Entry
-        //    using (var db = _provider.GetService<TestDbContext>())
-        //    {
-        //        //Arrange
-        //        db.SeedTestData();
-        //        var customer = db.Customers.First();
-        //        db.Customers.Remove(customer);
-        //        var deletedEntry = db.ChangeTracker.Entries().Where(p => p.State == EntityState.Deleted).First();
+        [Fact]
+        public void GetDeletedAuditLogsTest()
+        {
+            //This test ensures only one log is created for a Deleted Entity Entry
+            using (var db = _provider.GetService<TestDbContext>())
+            {
+                //Arrange
+                db.SeedTestData();
+                var customer = db.Customers.First();
+                db.Customers.Remove(customer);
+                var deletedEntry = db.ChangeTracker.Entries().Where(p => p.State == EntityState.Deleted).First();
 
-        //        //Act
-        //        var result = AuditLogBuilder.GetAddedAuditLogs(deletedEntry, _currentUser, EntityState.Deleted);
-        //        _auditLogList.Add(result);
+                //Act
+                var result = AuditLogBuilder.GetAddedAuditLogs(deletedEntry, _currentUser, EntityState.Deleted);
+                _auditLogList.Add(result);
 
-        //        //Assert
-        //        Assert.Equal(1, _auditLogList.Count());
-        //    }
-        //}
+                //Assert
+                Assert.Equal(1, _auditLogList.Count());
+            }
+        }
     }
 }
